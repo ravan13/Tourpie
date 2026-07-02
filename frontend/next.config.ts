ad19b52c0bbaa966bc230782aa74b5976cc463f2
ravan13 <ravanb13@gmail.com>
2026-07-02 19:53:26 +0400

    Remove Next.js rewrites

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,4 +1,4 @@
-import type { NextConfig } from "next";
+/*import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: process.cwd(),
@@ -19,13 +19,52 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  async rewrites() {
+  /*async rewrites() {
     return [
       {
         source: "/api/:path*",
         destination: "http://127.0.0.1:8080/:path*",
       },
     ];
+  },
+};*/
+/*async rewrites() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+
+  if (!apiUrl) {
+    return [];
+  }
+
+  return [
+    {
+      source: "/api/:path*",
+      destination: `${apiUrl}/:path*`,
+    },
+  ];
+}}
+
+export default nextConfig;*/
+
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: process.cwd(),
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
 };
 
